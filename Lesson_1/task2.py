57dc68fd3a47a0b1db261c5b670016f9e98eb362
alextranduil <alex.tranduil@gmail.com>
2022-10-26 18:20:12 +0300

    changing

--- Lesson_1/task2.py
+++ Lesson_1/task2.py
@@ -1,1 +1,15 @@
-
+firstFloatnumber= float(input("Enter first number: "))
+secondFloatnumber= float(input("Enter second number: "))
+operating= (input("Enter one of operations (+, -, *, /): "))
+if operating == "+":
+    Sum = firstFloatnumber + secondFloatnumber
+    print ("Sum: " + str (Sum))
+if operating == "-":
+    Difference = firstFloatnumber - secondFloatnumber
+    print ("Difference: " + str (Difference))
+if operating == "*":
+    Product = firstFloatnumber * secondFloatnumber
+    print ("Product: " + str (Product))
+if operating == "/":
+    Fraction = firstFloatnumber / secondFloatnumber
+    print ("Fraction: " + str (Fraction))
