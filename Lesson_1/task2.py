a7a5caa8a79cb463bdc7d22a69f6905661182f69
alextranduil <alex.tranduil@gmail.com>
2022-10-26 17:31:10 +0300

    changing

--- Lesson_1/task2.py
+++ Lesson_1/task2.py
@@ -1,13 +1,1 @@
-from tokenize import Floatnumber
 
-firstFloatNumber = float(input("Enter first number: "))
-secondFloatNumber = float(input("Enter second number: "))
-Sigh = (input("Enter arithmetic operation (+, -, *, /): "))
-if Sigh == "+":
- print ("Sum:" + str(firstFloatNumber + secondFloatNumber))
-if Sigh == "-":
- print ("Difference:" + str(firstFloatNumber - secondFloatNumber))
-if Sigh == "*":
- print ("Product:" + str(firstFloatNumber * secondFloatNumber))
-if Sigh == "/":
- print ("Part:" + str(firstFloatNumber / secondFloatNumber))
