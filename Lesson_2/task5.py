95254134925b0f8c784164bd73d54eaafc26f804
alextranduil <alex.tranduil@gmail.com>
2022-11-05 17:47:57 +0200

    without cycle and simplified

--- Lesson_2/task5.py
+++ Lesson_2/task5.py
@@ -1,14 +1,10 @@
 our_list = str(input("Enter numbers: "))
 split_our_list = our_list.split(", ")
 list_of_numbers = list(map(float, split_our_list))
-index = 0
-SumOfElementsExceptOfMinAndMax = 0 
-max = max(list_of_numbers)
-min = min(list_of_numbers)
-for index in range(0,len(list_of_numbers)):
-    if list_of_numbers[index] != max and list_of_numbers[index] != min:
-        SumOfElementsExceptOfMinAndMax = SumOfElementsExceptOfMinAndMax + list_of_numbers[index]
-    index = index + 1
-print ("Maximum: " + str(max))
-print ("Minimum: " + str(min))
+list_of_numbers.sort()
+print ("Maximum: " + str(list_of_numbers[-1]))
+list_of_numbers.pop(-1)
+print ("Minimum: " + str(list_of_numbers[0]))
+list_of_numbers.pop(0)
+SumOfElementsExceptOfMinAndMax = sum(list_of_numbers)
 print ("Sum of elements except of maximum and minimum: " + str(SumOfElementsExceptOfMinAndMax))
