08e8cc0d3d4ecf70d530b53e055e46884e15f45d
alextranduil <alex.tranduil@gmail.com>
2022-11-07 20:34:32 +0200

    Count all symbols

--- Lesson_2/task1.py
+++ Lesson_2/task1.py
@@ -1,12 +1,11 @@
-string = input("Enter string of liters: ")
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+input_string = input("Enter string of liters: ")
 result = {}
-for element in alphabet:
-  amount = string.count(element) # 1 - Count the number of each letter in a string.
-  if amount > 0:
-    print (str(element) +" - "+ str(amount)) # 2 - Print the number of each letter separately
-    result[element]=int(amount) 
+string = set(input_string)
+for element in string:
+  result[element] = input_string.count(element) # 1 - Count the number of each letter in a string.
 print (result)
+for key in result.keys():
+    print (str(key) +" - "+ str(result[key])) # 2 - Print the number of each letter separately
 sorted_values = sorted(result.values(), reverse=True) # 3 - Sort the number by the letters themselves
 sorted_result = {}
 for value in sorted_values:
